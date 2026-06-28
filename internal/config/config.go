17584837ced1cbd90d07e8d8f7ec6ccafdac3bac
kiskey <59226371+kiskey@users.noreply.github.com>
2026-06-28 16:22:44 -0700

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -51,7 +51,7 @@ func init() {
 
 	AddonID = "org.stremio.go.bitmagnet"
 	AddonName = "GoMagnet"
-	AddonVersion = "7.3.0" // Incremented for long-running show support and negate operator removal
+	AddonVersion = "7.3.1" // Incremented for robust season pack and filesStatus matching
 
 	RealDebridAPIKey = os.Getenv("REALDEBRID_API_KEY")
 	TmdbAPIKey = os.Getenv("TMDB_API_KEY")
