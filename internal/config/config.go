1a5bf2d4be11360d34bcd24b52883bf0a502e9ab
kiskey <59226371+kiskey@users.noreply.github.com>
2026-06-28 16:51:34 -0700

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -51,7 +51,7 @@ func init() {
 
 	AddonID = "org.stremio.go.bitmagnet"
 	AddonName = "GoMagnet"
-	AddonVersion = "7.3.3" // Incremented for conjoined language parsing protection fix
+	AddonVersion = "7.3.4" // Incremented for structural removal of fragile conjoined language splitter
 
 	RealDebridAPIKey = os.Getenv("REALDEBRID_API_KEY")
 	TmdbAPIKey = os.Getenv("TMDB_API_KEY")
